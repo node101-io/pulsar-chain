a9396e39f3751e8b6dd7a94fb1abd8ef9540961b
Yusuf <yusufozmis2@gmail.com>
2026-04-13 18:07:02 +0300

    feat: prepare proposal handler

--- abci/helpers.go
+++ abci/helpers.go
@@ -6,6 +6,10 @@ import (
 	keyregistrykeeper "github.com/node101-io/pulsar-chain/x/keyregistry/keeper"
 )
 
+var VoteExtMarker []byte = []byte("VOTEEXT:")
+
+const AcceptanceRatio float64 = 0.66
+
 type SecondaryKey struct {
 	SecretKey *keys.PrivateKey
 	PublicKey *keys.PublicKey
@@ -15,6 +19,7 @@ type AbciHandler struct {
 	secondaryKey      SecondaryKey
 	stakingKeeper     stakingkeeper.Keeper
 	keyregistryKeeper keyregistrykeeper.Keeper
+	votes             map[uint64]map[string][]byte // height -> minaAddress -> extension bytes
 }
 
 type VoteExtensionBody struct {
@@ -23,10 +28,37 @@ type VoteExtensionBody struct {
 	NextBlockHeight      int64
 }
 
+type payload struct {
+	Height uint64            `json:"height"`
+	Votes  map[string][]byte `json:"votes"`
+}
+
 func NewVoteExtHandler(secondaryKey SecondaryKey, stakingKeeper stakingkeeper.Keeper, keyregistryKeeper keyregistrykeeper.Keeper) *AbciHandler {
 	return &AbciHandler{
 		secondaryKey:      secondaryKey,
 		stakingKeeper:     stakingKeeper,
 		keyregistryKeeper: keyregistryKeeper,
 	}
+}
+
+func (h *AbciHandler) storeVote(height uint64, minaKey string, voteExt []byte) {
+
+	voteMap := h.votes[height]
+
+	voteMap[minaKey] = voteExt
+
+}
+
+func (h *AbciHandler) fetchVote(height uint64, minaKey string) []byte {
+
+	voteMap := h.votes[height]
+
+	return voteMap[minaKey]
+}
+
+func (h *AbciHandler) fetchVotes(height uint64) map[string][]byte {
+
+	voteMap := h.votes[height]
+
+	return voteMap
 }
