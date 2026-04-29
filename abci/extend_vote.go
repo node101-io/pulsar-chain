4e1727062d4f2dc7322244b18225e7d94885843e
Yusuf <yusufozmis2@gmail.com>
2026-04-29 13:31:43 +0300

    refactor: move functions to helpers and make vote extension compatible with spec

--- abci/extend_vote.go
+++ abci/extend_vote.go
@@ -1,15 +1,10 @@
 package vote_ext
 
 import (
-	"math/big"
-
 	abci "github.com/cometbft/cometbft/abci/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	stakingkeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
-	"github.com/node101-io/mina-signer-go/constants"
-	"github.com/node101-io/mina-signer-go/field"
-	"github.com/node101-io/mina-signer-go/keys"
-	"github.com/node101-io/mina-signer-go/poseidon"
+	keyregistrykeeper "github.com/node101-io/pulsar-chain/x/keyregistry/keeper"
 )
 
 const ActionsReducedRoot string = "pulsar"
@@ -19,86 +14,16 @@ type validatorInfo struct {
 	Power         int64
 }
 
-func MockSign(voteExtBody VoteExtensionBody) []byte {
-	return []byte{}
+type VoteExtensionBody struct {
+	NextValidatorSetHash []byte
+	CurrentStateRoot     []byte
+	CurrentBlockHeight   int64
 }
 
-func MockSignatureVerify(voteExtBody VoteExtensionBody, minaKey []byte, reducedRoot string) bool {
-	return true
-}
-
-func (h *AbciHandler) getValidatorSet(ctx sdk.Context, currentBlockHeight int64) ([]validatorInfo, error) {
-
-	historicalData, err := h.stakingKeeper.GetHistoricalInfo(ctx, currentBlockHeight)
-	if err != nil {
-		return nil, err
-	}
-
-	validatorSet := historicalData.Valset
-
-	var valInfo []validatorInfo
-
-	for _, validator := range validatorSet {
-
-		consAddr, err := validator.GetConsAddr()
-		if err != nil {
-			return nil, err
-		}
-
-		consPower := validator.ConsensusPower(sdk.DefaultPowerReduction)
-
-		valInfo = append(valInfo, validatorInfo{
-			ConsensusAddr: consAddr,
-			Power:         consPower,
-		})
-	}
-	return valInfo, nil
-}
-
-func (h *AbciHandler) calculateValidatorSetRoot(ctx sdk.Context, valInfo []validatorInfo, poseidonHash *poseidon.Poseidon) (*big.Int, error) {
-
-	input := []*big.Int{big.NewInt(0)}
-	merkleRoot := poseidonHash.Hash(input)
-
-	for _, validator := range valInfo {
-		input = []*big.Int{}
-
-		minaPubKeyExists, err := h.keyregistryKeeper.ValidatorCosmosToMinaHas(ctx, validator.ConsensusAddr)
-		if err != nil {
-			return nil, err
-		}
-		if !minaPubKeyExists {
-			return nil, err
-		}
-
-		minaPubKey, err := h.keyregistryKeeper.ValidatorGetCosmosToMina(ctx, validator.ConsensusAddr)
-		if err != nil {
-			return nil, err
-		}
-
-		MinaPublicKey, err := keys.PublicKey{}.FromAddress(string(minaPubKey))
-		if err != nil {
-			return nil, err
-		}
-
-		input = append(input, MinaPublicKey.X)
-		if MinaPublicKey.IsOdd {
-			input = append(input, big.NewInt(1))
-		} else {
-			input = append(input, big.NewInt(0))
-		}
-		power := new(big.Int).SetInt64(validator.Power)
-		input = append(input, power)
-
-		hashOfAddr := poseidonHash.Hash(input)
-
-		input = []*big.Int{merkleRoot, hashOfAddr}
-
-		merkleRoot = poseidonHash.Hash(input)
-	}
-
-	return merkleRoot, nil
-
+type AbciHandler struct {
+	secondaryKey      SecondaryKey
+	stakingKeeper     stakingkeeper.Keeper
+	keyregistryKeeper keyregistrykeeper.Keeper
 }
 
 func (h *AbciHandler) ExtendVoteHandler() sdk.ExtendVoteHandler {
@@ -107,28 +32,9 @@ func (h *AbciHandler) ExtendVoteHandler() sdk.ExtendVoteHandler {
 			return &abci.ResponseExtendVote{VoteExtension: []byte{}}, nil
 		}
 
-		twoBlocksBefore, err := stakingkeeper.Keeper.GetHistoricalInfo(h.stakingKeeper, ctx, req.GetHeight()-2)
-		if err != nil {
-			return &abci.ResponseExtendVote{VoteExtension: []byte{}}, err
-		}
-
-		nextBlockHeight := req.GetHeight() - 1
-
-		poseidonHash := poseidon.CreatePoseidon(*field.Fp, constants.PoseidonParamsKimchiFp)
-
-		nextValSet, err := h.getValidatorSet(ctx, nextBlockHeight)
+		body, err := h.constructVoteExtBody(ctx, req.GetHeight())
 		if err != nil {
 			return nil, err
-		}
-		nextValidatorSetHash, err := h.calculateValidatorSetRoot(ctx, nextValSet, poseidonHash)
-		if err != nil {
-			return nil, err
-		}
-
-		body := VoteExtensionBody{
-			NextBlockHeight:      nextBlockHeight,
-			CurrentStateRoot:     twoBlocksBefore.Header.AppHash,
-			NextValidatorSetHash: nextValidatorSetHash.Bytes(),
 		}
 
 		bz := MockSign(body)
