ae8e708ea00b7f191f587d5f1efc2aa3b7c2d3e8
Yusuf <yusufozmis2@gmail.com>
2026-04-29 21:34:12 +0300

    feat: initialize query for vote extension payload

--- x/votepersistence/module/autocli.go
+++ x/votepersistence/module/autocli.go
@@ -17,6 +17,13 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 					Use:       "params",
 					Short:     "Shows the parameters of the module",
 				},
+				{
+					RpcMethod:      "VoteExtBodyByHeight",
+					Use:            "vote-ext-body-by-height [block-height]",
+					Short:          "Query vote-ext-body-by-height",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{{ProtoField: "block_height"}},
+				},
+
 				// this line is used by ignite scaffolding # autocli/query
 			},
 		},
