f3d5d26c65383aa55e13cd2cf9fc376236e10f23
Yusuf <yusufozmis2@gmail.com>
2026-08-06 16:46:33 +0300

    chore:remove whitespace in query use

--- x/bridge/module/autocli.go
+++ x/bridge/module/autocli.go
@@ -19,14 +19,14 @@ func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 				},
 				{
 					RpcMethod:      "ActionsReducedRoot",
-					Use:            "actions-reduced-root ",
+					Use:            "actions-reduced-root",
 					Short:          "Query actions-reduced-root",
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{},
 				},
 
 				{
 					RpcMethod:      "LatestValidActionHashes",
-					Use:            "latest-valid-action-hashes ",
+					Use:            "latest-valid-action-hashes",
 					Short:          "Query latest-valid-action-hashes",
 					PositionalArgs: []*autocliv1.PositionalArgDescriptor{},
 				},
