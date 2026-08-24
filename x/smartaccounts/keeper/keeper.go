59f0c13802a4776d9494f54da806a6978a508704
Yusuf <yusufozmis2@gmail.com>
2026-08-24 21:04:35 +0300

    feat: implement AppendSessionKeyToSmartAccount method for future use in AddPublicKey tx

--- x/smartaccounts/keeper/keeper.go
+++ x/smartaccounts/keeper/keeper.go
@@ -1,13 +1,15 @@
 package keeper
 
 import (
+	"bytes"
+	"context"
 	"fmt"
 
 	"cosmossdk.io/collections"
 	"cosmossdk.io/core/address"
 	corestore "cosmossdk.io/core/store"
 	"github.com/cosmos/cosmos-sdk/codec"
-
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/node101-io/pulsar-chain/x/smartaccounts/types"
 )
 
@@ -72,4 +74,107 @@ func NewKeeper(
 // GetAuthority returns the module's authority.
 func (k Keeper) GetAuthority() []byte {
 	return k.authority
+}
+
+func (k Keeper) HasSmartAccount(ctx context.Context, identity []byte) (bool, error) {
+
+	if identity == nil {
+		return false, types.ErrNilIdentity
+	}
+
+	if len(identity) == 0 {
+		return false, types.ErrNilIdentity
+	}
+
+	return k.smartAccounts.Has(ctx, identity)
+}
+
+func (k Keeper) AppendSessionKeyToSmartAccount(ctx context.Context, identity []byte, key types.SessionKey) error {
+
+	if identity == nil {
+		return types.ErrNilIdentity
+	}
+
+	if len(identity) == 0 {
+		return types.ErrNilIdentity
+	}
+
+	if key.PublicKey == nil {
+		return types.ErrNilPublicKey
+	}
+
+	if len(key.PublicKey) != types.SessionPublicKeySize {
+		return types.ErrPublicKeyInvalidLength
+	}
+
+	if key.ExpiresAtHeight == 0 {
+		return types.ErrInvalidExpirationHeight
+	}
+
+	exists, err := k.HasSmartAccount(ctx, identity)
+	if err != nil {
+		return err
+	}
+
+	if !exists {
+		err := k.smartAccounts.Set(ctx, identity, types.SmartAccount{
+			SessionKeys: []types.SessionKey{
+				key,
+			},
+		})
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	acc, err := k.smartAccounts.Get(ctx, identity)
+	if err != nil {
+		return err
+	}
+
+	for _, existingKey := range acc.SessionKeys {
+		if bytes.Equal(existingKey.PublicKey, key.PublicKey) &&
+			existingKey.ExpiresAtHeight == key.ExpiresAtHeight {
+			return types.ErrSessionKeyAlreadyExists
+		}
+	}
+
+	acc.SessionKeys = append(acc.SessionKeys, key)
+
+	if err := k.smartAccounts.Set(ctx, identity, types.SmartAccount{
+		SessionKeys: acc.SessionKeys,
+	}); err != nil {
+		return err
+	}
+
+	return k.pruneSessionKey(ctx, identity)
+}
+
+func (k Keeper) pruneSessionKey(ctx context.Context, identity []byte) error {
+
+	var validSessionKeys []types.SessionKey
+
+	smartAcc, err := k.smartAccounts.Get(ctx, identity)
+	if err != nil {
+		return err
+	}
+
+	currentBlockHeight := uint64(sdk.UnwrapSDKContext(ctx).BlockHeight())
+
+	for _, key := range smartAcc.SessionKeys {
+
+		if currentBlockHeight >= key.ExpiresAtHeight {
+			continue
+		}
+		validSessionKeys = append(validSessionKeys, key)
+	}
+
+	if err := k.smartAccounts.Set(ctx, identity, types.SmartAccount{
+		SessionKeys: validSessionKeys,
+	}); err != nil {
+		return err
+	}
+
+	return nil
 }
--- x/smartaccounts/types/errors.go
+++ x/smartaccounts/types/errors.go
@@ -8,5 +8,10 @@ import (
 
 // x/smartaccounts module sentinel errors
 var (
-	ErrInvalidSigner = errors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	ErrInvalidSigner           = errors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
+	ErrNilPublicKey            = errors.Register(ModuleName, 1101, "nil public key")
+	ErrPublicKeyInvalidLength  = errors.Register(ModuleName, 1102, "public key invalid length")
+	ErrInvalidExpirationHeight = errors.Register(ModuleName, 1103, "invalid expiration height")
+	ErrNilIdentity             = errors.Register(ModuleName, 1105, "nil identity")
+	ErrSessionKeyAlreadyExists = errors.Register(ModuleName, 1106, "session key already exists")
 )
